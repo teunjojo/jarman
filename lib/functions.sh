720edfb9920ec2b5a1163fa00d789f94ee322477
Teuntje Kuipers <mail@teunjojo.com>
2025-07-16 22:18:39 +0200

    Fix typo `files` to `file` in functions.sh

--- lib/functions.sh
+++ lib/functions.sh
@@ -63,7 +63,7 @@ register_jar() {
 
 	while true; do
 		echo $seperator
-		echo -e "Unregistered JAR files found!"
+		echo -e "Unregistered JAR file found!"
 		read -p "Do you want to register '$jar_filename'? [Y/n] " -r
 		if [[ $REPLY =~ ^[Nn]$ ]]; then # If 'N' or 'n' then dont ask for user input
 			break
