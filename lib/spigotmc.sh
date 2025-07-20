a421a7118815924567b19b052a19e6b090216c52
Teuntje Kuipers <mail@teunjojo.com>
2025-07-20 23:43:59 +0200

    Add framework for SpigotMC support

---
+++ lib/spigotmc.sh
@@ -0,0 +1,80 @@
+# Script should be sourced, not executed
+if [ "${BASH_SOURCE-}" = "$0" ] || [ -z "${BASH_SOURCE-}" ]; then
+	echo "This script must be sourced, not executed."
+	exit 1
+fi
+
+#######################################
+# Updates a JAR to the latest version via SpigotMC
+# Globals:
+#	None
+# Arguments:
+#   jar_json
+#######################################
+spigotmc_update() {
+	local jar_json=$1
+	local jar_loader=$(echo "$jar_json" | jq -r '.loader')
+	[ -z "$jar_loader" ] && error_handler "JAR loader not set"
+	local jar_project=$(echo "$jar_json" | jq -r '.project')
+	[ -z "$jar_project" ] && error_handler "JAR project not set"
+	local jar_filename=$(echo $jar_json | jq -r '.filename' 2>/dev/null)
+	local artifact_number=$(echo $jar_json | jq -r '.artifactNumber' 2>/dev/null)
+	local latest_version=$(modrinth_get_version "$jar_json")
+
+	local download_url=""
+
+	curl -sS -L -o "$jar_filename" "$download_url"
+
+	tmp_file=$(mktemp)
+	jq --arg filename "$jar_filename" --arg version "$latest_version" 'map(if .filename == $filename then .version = $version else . end)' "$cache_file" >"$tmp_file" && mv "$tmp_file" "$cache_file"
+}
+
+#######################################
+# Returns the latest version of a JAR via SpigotMC
+# Globals:
+#	None
+# Arguments:
+#   jar_json
+#######################################
+spigotmc_get_version() {
+	local jar_json=$1
+	local jar_loader=$(echo "$jar_json" | jq -r '.loader')
+	[ -z "$jar_loader" ] && error_handler "JAR loader not set"
+	local jar_project=$(echo "$jar_json" | jq -r '.project')
+	[ -z "$jar_project" ] && error_handler "JAR project not set"
+
+	local spigotmc_latest_version=""
+	[ -z "$spigotmc_latest_version" ] && error_handler "Failed to retrieve latest version number"
+
+	echo "$spigotmc_latest_version"
+}
+
+#######################################
+# Curl for SpigotMC
+# Globals:
+#	None
+# Arguments:
+#   jar_json
+#######################################
+spigotmc_curl() {
+	local url=$1
+	local response=$(mktemp)
+	curl_opts=(-s -w "%{http_code}" -o "$response")
+
+	status=$(curl "${curl_opts[@]}" "$url")
+
+	case "$status" in
+	"200") ;;
+
+	"403")
+		error_handler "$(cat "$response" | jq '.message')"
+		;;
+
+	"404")
+		error_handler "Not Found"
+		;;
+	esac
+
+	cat "$response" | jq .
+	rm "$response"
+}
